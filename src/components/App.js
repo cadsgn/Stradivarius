fc0b0ea87e9dce486545273d017dc195665bd41e
cadsgn <krlos.ad@gmail.com>
2021-06-22 19:14:41 +0200

    project setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bon dia
         </p>
         <a
           className="App-link"
@@ -19,6 +16,7 @@ function App() {
         </a>
       </header>
     </div>
+    </>
   );
 }
 
---
+++ src/theme/globalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        font-family: 'Roboto',  sans-serif;
+        background-color: #e0e0e0;
+
+    }
+`;
+ 
+export default GlobalStyle;
